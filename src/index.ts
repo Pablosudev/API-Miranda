5c72b14b88a184655cb66334264601392b2aecbf
Pablosudev <pablo.losu.dev@gmail.com>
2025-05-23 13:37:21 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { roomsRouter } from "./Controllers/room";
 import { bookingsRouter } from "./Controllers/booking";
 import { contactRouter } from "./Controllers/contact";
@@ -13,8 +13,17 @@ const app = express();
 // const port = 3001;
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
-const cors = require("cors");
-app.use(cors());
+app.use(function corsMiddleware(req: Request, res: Response, next: NextFunction): void {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 connectDB();
 
 const swaggerOptions = {
